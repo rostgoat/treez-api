c724330d854cf488d76daa631d62d871c81b58ea
Rost <rdavidoff@hotmail.ca>
2020-01-12 17:53:40 -0800

    inventory DTO; functions comments in inventory service

--- src/inventory/inventory.service.ts
+++ src/inventory/inventory.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { InventoryEntity } from './inventory.entity';
 import { InjectRepository } from '@nestjs/typeorm';
+import { InventoryDTO } from './inventory.dto';
 
 /**
  * Inventory Service
@@ -10,26 +11,46 @@ import { InjectRepository } from '@nestjs/typeorm';
 export class InventoryService {
     constructor(@InjectRepository(InventoryEntity) private inventoryRepository: Repository<InventoryEntity>) {}
 
-    async add(data) {
+    /**
+     * Add invetory item 
+     * @param data Object
+     */
+    async add(data: InventoryDTO) {
         const inventoryItem = await this.inventoryRepository.create(data)
         await this.inventoryRepository.save(inventoryItem)
         return inventoryItem
     }
 
-    async edit(id: string, data) {
+    /**
+     * Update inventory item
+     * @param id String
+     * @param data Object
+     */
+    async edit(id: string, data: Partial<InventoryDTO>) {
         await this.inventoryRepository.update({id},data)
         return await this.inventoryRepository.findOne({id})
     }
 
+    /**
+     * Delete inventory item
+     * @param id String
+     */
     async delete(id: string) {
         await this.inventoryRepository.delete({id})
         return {deleted: true}
     }
 
+    /**
+     * Return specific invetory item specified by id
+     * @param id String
+     */
     async getOne(id: string) {
         return await this.inventoryRepository.findOne({ where: { id } })
     }
 
+    /**
+     * Return all inventory items
+     */
     async getAll() {
         return await this.inventoryRepository.find();
     }
