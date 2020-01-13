2bfbb10f0220b3b8a107fcc5353761b547458c1c
Rost <rdavidoff@hotmail.ca>
2020-01-12 18:02:06 -0800

    controller methods and comments for inventory

--- src/inventory/inventory.controller.ts
+++ src/inventory/inventory.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get, Post, Put, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Body, Param } from '@nestjs/common';
 import { InventoryService } from './inventory.service';
+import { InventoryDTO } from './inventory.dto';
 
 /**
  * Inventory Controller
@@ -7,20 +8,49 @@ import { InventoryService } from './inventory.service';
 @Controller('inventory')
 export class InventoryController {
     constructor(private inventoryService: InventoryService) {}
+
+    /**
+     * Create an inventory object
+     * @param data Object
+     */
     @Post()
-    createInventoryItem() {}
-
-    @Get()
-    getAllInventoryItems() {}
-
-    @Get(':id')
-    getInventoryItem() {
-
+    createInventoryItem(@Body() data: InventoryDTO) {
+        return this.inventoryService.add(data)
     }
+
+    /**
+     * Update inventory item
+     * @param id String 
+     * @param data Object
+     */
     @Put(':id')
-    updateInventoryItem() {
-
+    updateInventoryItem(@Param('id') id: string, @Body() data: Partial<InventoryDTO>) {
+        return this.inventoryService.edit(id, data)
     }
+
+    /**
+     * Delete invetory item
+     * @param id String
+     */
     @Delete(':id')
-    deleteInventoryItem() {}
+    deleteInventoryItem(@Param('id') id: string) {
+        return this.inventoryService.delete(id)
+    }
+
+    /**
+     * Get all inventory items
+     */
+    @Get()
+    getAllInventoryItems() {
+        return this.inventoryService.getAll()
+    }
+
+    /**
+     * Get a specific invetory item
+     * @param id String
+     */
+    @Get(':id')
+    getInventoryItem(@Param('id') id: string) {
+        return this.inventoryService.getOne(id)
+    }
 }
