5ed6f9d28d2c2344aec8cd2c405ceb610c479ae8
Rost <rdavidoff@hotmail.ca>
2020-01-12 17:48:15 -0800

    inventory service operations

--- src/inventory/inventory.service.ts
+++ src/inventory/inventory.service.ts
@@ -1,9 +1,36 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { InventoryEntity } from './inventory.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 
 /**
  * Inventory Service
  */
 @Injectable()
 export class InventoryService {
-    constructor
+    constructor(@InjectRepository(InventoryEntity) private inventoryRepository: Repository<InventoryEntity>) {}
+
+    async add(data) {
+        const inventoryItem = await this.inventoryRepository.create(data)
+        await this.inventoryRepository.save(inventoryItem)
+        return inventoryItem
+    }
+
+    async edit(id: string, data) {
+        await this.inventoryRepository.update({id},data)
+        return await this.inventoryRepository.findOne({id})
+    }
+
+    async delete(id: string) {
+        await this.inventoryRepository.delete({id})
+        return {deleted: true}
+    }
+
+    async getOne(id: string) {
+        return await this.inventoryRepository.findOne({ where: { id } })
+    }
+
+    async getAll() {
+        return await this.inventoryRepository.find();
+    }
 }
