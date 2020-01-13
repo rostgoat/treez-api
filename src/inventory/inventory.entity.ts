fed4d43db0d07248c84105da74a3108aa106afa2
Rost <rdavidoff@hotmail.ca>
2020-01-12 18:45:16 -0800

    name unique constraint

--- src/inventory/inventory.entity.ts
+++ src/inventory/inventory.entity.ts
@@ -1,13 +1,14 @@
-import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm'
+import { Entity, PrimaryGeneratedColumn, Column, Unique } from 'typeorm'
 
 /**
  * Inventory Entity
  */
 @Entity('inventory')
+@Unique(['name'])
 export class InventoryEntity {
     @PrimaryGeneratedColumn('uuid') id: string
     @Column('text') name: string
     @Column('text') description: string
-    @Column('int8') price: number
+    @Column('float4') price: number
     @Column('int8') quantity_available: number
 }
